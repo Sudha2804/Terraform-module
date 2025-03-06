8458af7da8db812ac354d0074ca0f71955ced29b
Sudha G C <158569395+Sudha2804@users.noreply.github.com>
2025-03-06 18:02:12 +0530

    Update variables.tf

--- ec2_instance/variables.tf
+++ ec2_instance/variables.tf
@@ -1,35 +1,7 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "cidr_block" {
-  description = "CIDR block for the VPC"
-  type        = string
-}
-
-variable "subnet_cidr_block" {
-  description = "CIDR block for the subnet"
-  type        = string
-}
-
-variable "allowed_ssh_ip" {
-  description = "IP range allowed for SSH access"
-  type        = string
-}
-
-variable "ami" {
-  description = "AMI ID for EC2 instance"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "Instance type for EC2"
-  type        = string
-}
-
-variable "key_name" {
-  description = "SSH key pair name"
-  type        = string
-}
-
+variable "region" {}
+variable "cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "allowed_ssh_ip" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "key_name" {}
