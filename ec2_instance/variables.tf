88ae0e486a7720dec63609139c32eeacd1cee13f
Sudha G C <158569395+Sudha2804@users.noreply.github.com>
2025-03-06 18:11:49 +0530

    Update variables.tf

--- ec2_instance/variables.tf
+++ ec2_instance/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {}
 variable "cidr_block" {}
 variable "subnet_cidr_block" {}
-variable "allowed_ssh_ip" {}
+#variable "allowed_ssh_ip" {}
 variable "ami" {}
 variable "instance_type" {}
 variable "key_name" {}
