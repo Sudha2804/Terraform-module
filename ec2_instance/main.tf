fdbd072f81b3e67092d8ea0780b5b775e89887f9
Sudha G C <158569395+Sudha2804@users.noreply.github.com>
2025-03-09 15:22:27 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
   
 }
 module "ec2_instance" {
-  source        = "git::https://github.com/sonyhl05/assignment_terraform.git//modules/ec2_instance"
+  source        = "github.com/Sudha2804/Terraform-module/ec2_instance"
   region        = var.region
   instance_type = var.instance_type
 }
